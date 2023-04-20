f73fc02920787a96a83d52c75396119b45640fe9
jasan2303 <89679528+jasan2303@users.noreply.github.com>
2023-04-20 13:32:47 -0600

    Update BinarySearchInsertPosition.c

--- BinarySearchInsertPosition.c
+++ BinarySearchInsertPosition.c
@@ -1,27 +1,23 @@
 int searchInsert(int* nums, int numsSize, int target){
    // Optimized approach is BS
    int low =0, high = numsSize-1;
-    int idx = numsSize;
+   
+   // ****** VERY IMP TO DO THis *******
+   int idx = numsSize;
 
     while( low <= high)
     {
         int mid = low + (high - low)/ 2;
-       
-        if(nums[mid] >= target)
-            {
-                idx = mid;
-                
-                high = mid - 1;
-            }
-            else
-            {
-                low = mid + 1;
-            }
+         if(nums[mid] >= target)
+         {
+             idx = mid;
+             high = mid - 1;
+         }
+         else
+         {
+             low = mid + 1;
+         }
     }
-
-    
-    return idx;
-
-
-
+   
+  return idx;
 }
